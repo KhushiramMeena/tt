3d9a796df93ee0ca2753889dafbfe36399796838
Khushiram Meena <80107998+KhushiramMeena@users.noreply.github.com>
2023-10-28 05:07:21 +0530

    Update hero.ts

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -3,7 +3,7 @@ import { resumeFileName } from '@/lib/utils/config'
 
 export const heroSection: HeroSectionType = {
     subtitle: 'Hi, my name is',
-    title: 'vatsal singh.',
+    title: 'Khushiram Meena',
     tagline: 'I create visually pleasing interfaces for the web.',
     description:
     "I'm a passionate Front-End web developer having an experience of web applications with React.js & Next.js with TypeScript & TailwindCSS and UI/UX designing.",
